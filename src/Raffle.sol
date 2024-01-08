5c2a2a62c2a1e88eb9c469a2f45114ba87d2b8f0
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-07 23:42:24 -0500

    interval refactor

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -109,7 +109,7 @@ contract Raffle is VRFConsumerBaseV2 {
      * 4. The subscription is funded with LINK
      */
     function checkUpkeep(bytes memory) public view returns(bool upkeepNeeded, bytes memory){
-        bool timeHasPassed = ((block.timestamp - s_lastTimestamp) > i_interval);
+        bool timeHasPassed = (block.timestamp - s_lastTimestamp) >= i_interval;
     }
 
     /** Getter Functions */
