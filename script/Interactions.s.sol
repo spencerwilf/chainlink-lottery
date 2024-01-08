4e8b33ee9cc43643624ba2192fb99911c71541a2
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 13:04:00 -0500

    started funding

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -34,3 +34,8 @@ contract CreateSubscription is Script {
         return createSubscriptionUsingConfig();
     }
 }
+
+
+contract FundSubscription is Script {
+    uint96 public constant FUND_AMOUNT = 3 ether;
+}
