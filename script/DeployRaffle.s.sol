4dc7c9d9c9731b84eb1d26cd018b80384a90858a
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 11:43:39 -0500

    helper

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.21;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle) {}
