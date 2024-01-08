2fb8651867aada95a931485753eb4b256ff31964
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 12:48:40 -0500

    create subscription

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract CreateSubscription is Script {
-    
+
+    function createSubscriptionUsingConfig() public returns(uint64) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        (,
+        ,
+        address vrfCoordinator,
+        ,
+        ,
+        ) = helperConfig.activeNetwork();
+
+        return createSubscription(vrfCoordinator);
+    }
+
+    function createSubscription(address vrfCoordinator) public returns(uint64) {
+        console.log("Creating subscription on ChainId: ", block.chainid);
+        vm.startBroadcast();
+
+        vm.stopBroadcast();
+    }
+
+    function run() external returns(uint64) {
+        return createSubscriptionUsingConfig();
+    }
 }
