ff117625739a8cf4631e834c0ae5c92f1f02b4ce
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 11:21:48 -0500

    network config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,5 +4,14 @@ pragma solidity 0.8.21;
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
-    
+
+    struct NetworkConfig {
+        uint entranceFee; 
+        uint interval; 
+        address vrfCoordinator; 
+        bytes32 gasLane; 
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
 }
