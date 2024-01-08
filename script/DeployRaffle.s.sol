ea88a626c618b29fe64089cd71050970546d3746
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 11:48:26 -0500

    helper config deconstruct

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -6,5 +6,17 @@ import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        (uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit) = helperConfig.activeNetwork();
+
+        
+    }
 }
