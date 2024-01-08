c97fc09bc3d06473ecec696214bd0d90c7ee9a9b
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-08 11:54:20 -0500

    imports

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
 
 contract RaffleTest is Test {
-    
+
 }
